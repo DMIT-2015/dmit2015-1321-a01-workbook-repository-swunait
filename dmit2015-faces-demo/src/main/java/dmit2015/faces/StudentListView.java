f264ec9a01e0f69a117e93b5539b71746b901707
Sam Wu <swu@nait.ca>
2023-09-14 14:50:17 -0600

    End of Sep 14 class

---
+++ dmit2015-faces-demo/src/main/java/dmit2015/faces/StudentListView.java
@@ -0,0 +1,40 @@
+package dmit2015.faces;
+
+import dmit2015.restclient.Student;
+import dmit2015.restclient.StudentMpRestClient;
+import lombok.Getter;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.omnifaces.cdi.ViewScoped;
+import org.omnifaces.util.Messages;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Map;
+
+
+@Named("currentStudentListView")
+@ViewScoped
+public class StudentListView implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    @RestClient
+    private StudentMpRestClient _studentMpRestClient;
+
+    @Getter
+    private Map<String, Student> studentMap;
+
+    @PostConstruct  // After @Inject is complete
+    public void init() {
+        try {
+            studentMap = _studentMpRestClient.findAll();
+        } catch (Exception ex) {
+            Messages.addGlobalError(ex.getMessage());
+        }
+    }
+}
---
+++ dmit2015-faces-demo/src/main/java/dmit2015/restclient/Student.java
@@ -0,0 +1,13 @@
+package dmit2015.restclient;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+}
