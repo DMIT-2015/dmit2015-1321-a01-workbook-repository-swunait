9e0a2e3a909ac4d71bb7cdf3786c59677bdcaae2
Sam Wu <swu@nait.ca>
2023-10-27 15:13:47 -0600

    Added example on how to check for child records when deleting.

--- oracle-hr-app/src/main/java/dmit2015/persistence/CountryRepository.java
+++ oracle-hr-app/src/main/java/dmit2015/persistence/CountryRepository.java
@@ -13,4 +13,21 @@ public class CountryRepository extends AbstractJpaRepository<Country, String> {
         super(Country.class);
     }
 
+    public void delete(Country existingCountry) {
+        // Check if there are any child entity (Location) associated with this parent entity (Country)
+        long locationCount = getEntityManager().createQuery("""
+select count(l)
+from Location l
+where l.countryId = :countryIdValue
+""", Long.class)
+                .setParameter("countryIdValue", existingCountry.getCountryId())
+                .getSingleResult();
+        if (locationCount > 0) {
+            String message = String.format("This record cannot be deleted as there are %d associated records.", locationCount);
+            throw new RuntimeException(message);
+        }
+
+        super.delete(existingCountry);
+    }
+
 }
